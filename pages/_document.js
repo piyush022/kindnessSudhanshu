771c0ea5e41940b5a1b4a62ff02fe8a7cd098af1
piyush022 <piyushverma022@gmail.com>
2023-10-25 20:56:24 +0530

    added meta in document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,38 +4,29 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <NextSeo
-          title="The Kindness Campaign"
-          description="The Kindness Campaign"
-          canonical="https://kindness-omega.vercel.app"
-          openGraph={{
-            url: `https://kindness-omega.vercel.app`,
-            title: {},
-            description: {},
-            images: [
-              {
-                url: "/logo.png",
-                width: 800,
-                height: 600,
-                alt: "Og Image Alt",
-                type: "image/jpeg",
-              },
-              {
-                url: "/logo.png",
-                width: 900,
-                height: 800,
-                alt: "Og Image Alt Second",
-                type: "image/jpeg",
-              },
-            ],
-            siteName: "KindnessCampaign",
-          }}
-          twitter={{
-            handle: "@handle",
-            site: "@site",
-            cardType: "summary_large_image",
-          }}
+        <meta name="description" content="Kindness Campaign" />
+
+        <meta
+          property="og:title"
+          content=" Kindness Campaign"
+          class="yoast-seo-meta-tag"
         />
+        <meta
+          property="og:description"
+          content="Kindness Campaign ! The Kindness Campaign creates local leaders, involved community members, engaged businesses and at-promise young adults through leadership of impact initiatives."
+          class="yoast-seo-meta-tag"
+        />
+        <meta property="og:url" content="https://kindness-omega.vercel.app/" />
+        <meta property="og:site_name" content="Kindness Campaign" />
+        <meta
+          property="article:modified_time"
+          content="2023-09-18T05:45:43+00:00"
+        />
+        <meta property="og:image" content="/logo.png" />
+        <meta property="og:image:width" content="1024" />
+        <meta property="og:image:height" content="413" />
+        <meta property="og:image:type" content="image/png" />
+        <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <body>
         <Main />
